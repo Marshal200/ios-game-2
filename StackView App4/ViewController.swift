78ed70e13c900609ba2f92e6e99b03010196eb0b
test <test@MACPC271423.local>
2019-02-22 12:03:08 +0530

    first commit

---
+++ StackView App4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StackView App4
+//
+//  Created by test on 13/02/19.
+//  Copyright © 2019 cognizant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
